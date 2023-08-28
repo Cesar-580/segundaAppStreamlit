a2c934e8156c79acab566602ea954dc69c74509b
cAos <65823540+Cesar-580@users.noreply.github.com>
2023-08-28 16:40:56 -0500

    Conversion de temperaturas conversiones.py

--- conversiones.py
+++ conversiones.py
@@ -14,3 +14,24 @@ conversion_seleccionada = st.sidebar.selectbox("Seleccione el tipo de conversió
 
 # Mostrar el tipo de conversión seleccionado
 st.sidebar.write(f"Ha seleccionado: {conversion_seleccionada}")
+
+# Realizar conversiones de temperatura
+if conversion_seleccionada == "Conversiones de temperatura":
+    tipo_conversion = st.selectbox("Seleccione el tipo de conversión:",
+                                   ["Celsius a Fahrenheit", "Fahrenheit a Celsius",
+                                    "Celsius a Kelvin", "Kelvin a Celsius"])
+    
+    valor_original = st.number_input("Ingrese el valor a convertir:")
+    
+    if tipo_conversion == "Celsius a Fahrenheit":
+        valor_convertido = (valor_original * 9/5) + 32
+        st.write(f"{valor_original} °C equivale a {valor_convertido} °F")
+    elif tipo_conversion == "Fahrenheit a Celsius":
+        valor_convertido = (valor_original - 32) * 5/9
+        st.write(f"{valor_original} °F equivale a {valor_convertido} °C")
+    elif tipo_conversion == "Celsius a Kelvin":
+        valor_convertido = valor_original + 273.15
+        st.write(f"{valor_original} °C equivale a {valor_convertido} K")
+    elif tipo_conversion == "Kelvin a Celsius":
+        valor_convertido = valor_original - 273.15
+        st.write(f"{valor_original} K equivale a {valor_convertido} °C")
