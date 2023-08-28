760e5b897af603a8cb0a76ba64b27e192d3c558b
cAos <65823540+Cesar-580@users.noreply.github.com>
2023-08-28 16:37:21 -0500

    primera parte (Scroll de conversiones)conversiones.py

--- conversiones.py
+++ conversiones.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+# Título y autor de la app
+st.title("App de Conversiones")
+st.write("Esta app fue elaborada por Cesar Augusto Ospina Muñoz.")
+
+# Menú desplegable para seleccionar el tipo de conversión
+conversion_seleccionada = st.sidebar.selectbox("Seleccione el tipo de conversión:",
+                                               ["Conversiones de temperatura", "Conversiones de longitud",
+                                                "Conversiones de peso/masa", "Conversiones de volumen",
+                                                "Conversiones de tiempo", "Conversiones de velocidad",
+                                                "Conversiones de área", "Conversiones de energía",
+                                                "Conversiones de presión", "Conversiones de tamaño de datos"])
+
+# Mostrar el tipo de conversión seleccionado
+st.sidebar.write(f"Ha seleccionado: {conversion_seleccionada}")
