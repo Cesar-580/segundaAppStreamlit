ccaac36deea9ab9ca0e818dd9e572676963f8bc1
cAos <65823540+Cesar-580@users.noreply.github.com>
2023-08-28 16:44:08 -0500

    se agragaron las de longitud conversiones.py

--- conversiones.py
+++ conversiones.py
@@ -35,3 +35,24 @@ if conversion_seleccionada == "Conversiones de temperatura":
     elif tipo_conversion == "Kelvin a Celsius":
         valor_convertido = valor_original - 273.15
         st.write(f"{valor_original} K equivale a {valor_convertido} °C")
+
+elif conversion_seleccionada == "Conversiones de longitud":
+    tipo_conversion = st.selectbox("Seleccione el tipo de conversión:",
+                                   ["Pies a metros", "Metros a pies",
+                                    "Pulgadas a centímetros", "Centímetros a pulgadas"])
+    
+    valor_original = st.number_input("Ingrese el valor a convertir:")
+    
+    if tipo_conversion == "Pies a metros":
+        valor_convertido = valor_original * 0.3048
+        st.write(f"{valor_original} pies equivale a {valor_convertido} metros")
+    elif tipo_conversion == "Metros a pies":
+        valor_convertido = valor_original / 0.3048
+        st.write(f"{valor_original} metros equivale a {valor_convertido} pies")
+    elif tipo_conversion == "Pulgadas a centímetros":
+        valor_convertido = valor_original * 2.54
+        st.write(f"{valor_original} pulgadas equivale a {valor_convertido} centímetros")
+    elif tipo_conversion == "Centímetros a pulgadas":
+        valor_convertido = valor_original / 2.54
+        st.write(f"{valor_original} centímetros equivale a {valor_convertido} pulgadas")
+
